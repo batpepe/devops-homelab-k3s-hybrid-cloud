b9b74f29f325c2766a5f4bd5020d23f64e22d061
batpepe <oskostya25@gmail.com>
2026-05-19 22:09:55 +0300

    test: true GitOps automated

--- apps/flask-app/app.py
+++ apps/flask-app/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def hello():
     hostname = socket.gethostname()
     version = os.environ.get('APP_VERSION', '1.0.0')
-    return f"<h1>Hello from CI/CD Pipeline! 🚀</h1><p>Running on pod: {hostname}</p><p>Version: {version}</p>"
+    return f"<h1>Hello from TRUE GITOPS! 🚀</h1><p>Running on pod: {hostname}</p><p>Version: {version}</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
